d31723e7901b8b80b389233c77a763dfb876cf65
Zinaida <138328446+zinula20@users.noreply.github.com>
2023-07-29 18:47:44 +0300

    Add files via upload

---
+++ hm4.js
@@ -0,0 +1,18 @@
+var a, b, c, d, e
+a=prompt("Please enter your first name: ");
+b=prompt("Please enter your last name: ");
+c=prompt("Please enter your address: ");
+d=prompt("Please enter your birth year: ");
+e=2023-d;
+document.write('<h3 style class="circle-1">First name: </br>', a, 
+'<h3 style class="circle-2">Last name: </br>', b, 
+'<h3 style class="circle-3">Address: </br>', c, '<h3 style class="circle-4">Year of birth: </br>', d, 
+'<h3 style class="circle-5">Age: </br>', e);
+
+
+
+
+
+
+
+
